c0decd849cc735971d0d17a27e1d2e672a8ab95f
AmAdev <aaananchai@gmail.com>
2019-09-04 00:08:29 +0700

    add Loading Spinner

---
+++ LTMAN_GRAPHQL/LTMAN_GRAPHQL/Loading.swift
@@ -0,0 +1,15 @@
+//
+//  Loading.swift
+//  LTMAN_GRAPHQL
+//
+//  Created by Ananchai Mankhong on 3/9/2562 BE.
+//  Copyright © 2562 Ananchai Mankhong. All rights reserved.
+//
+
+import UIKit
+
+class Loading: UICollectionViewCell {
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+}
